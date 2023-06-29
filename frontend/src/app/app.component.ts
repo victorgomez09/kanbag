3f480fe6fd1513a34a4f9127be6ff749116edfdc
Víctor Gómez <victorgomez1209@outlook.es>
2023-06-29 22:07:06 +0200

    add form view

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  private primeNgConfig: PrimeNGConfig = inject(PrimeNGConfig);
+
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
+  }
 }
--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,25 +1,49 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ModelFormGroup } from 'src/app/types';
+import { RippleModule } from 'primeng/ripple';
+
+interface Login {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ButtonModule, CardModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    RippleModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
-  private fb: FormBuilder = inject(FormBuilder)
-  public formGroup: FormGroup;
+  private fb: FormBuilder = inject(FormBuilder);
+  public loginForm: ModelFormGroup<Login>;
 
   constructor() {
-    this.formGroup = this.fb.group({
+    this.loginForm = this.fb.nonNullable.group({
       email: ['', [Validators.email, Validators.required]],
-      password: ['', Validators.required]
-    })
+      password: ['', Validators.required],
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
   }
 }
