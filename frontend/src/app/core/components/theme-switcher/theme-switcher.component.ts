35d3627904f3e5e84fdcaae03947ba6640c36eae
Victin09 <victorgomez0937@gmail.com>
2023-07-28 11:57:39 +0000

    add theme switcher

--- frontend/src/app/core/components/theme-switcher/theme-switcher.component.ts
+++ frontend/src/app/core/components/theme-switcher/theme-switcher.component.ts
@@ -1,138 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { themeChange } from 'theme-change';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-theme-switcher',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './theme-switcher.component.html',
   styleUrls: ['./theme-switcher.component.scss']
 })
 export class ThemeSwitcherComponent {
 
-  public themes: { name: string; id: string; }[];
+  public themeSwitch: boolean;
 
   constructor() {
-    themeChange()
+    this.themeSwitch = localStorage.getItem("theme") === "dark";
+  }
 
-    this.themes = [
-      {
-        name: "🌝  light",
-        id: "light",
-      },
-      {
-        name: "🌚  dark",
-        id: "dark",
-      },
-      {
-        name: "🧁  cupcake",
-        id: "cupcake",
-      },
-      {
-        name: "🐝  bumblebee",
-        id: "bumblebee",
-      },
-      {
-        name: "✳️  Emerald",
-        id: "emerald",
-      },
-      {
-        name: "🏢  Corporate",
-        id: "corporate",
-      },
-      {
-        name: "🌃  synthwave",
-        id: "synthwave",
-      },
-      {
-        name: "👴  retro",
-        id: "retro",
-      },
-      {
-        name: "🤖  cyberpunk",
-        id: "cyberpunk",
-      },
-      {
-        name: "🌸  valentine",
-        id: "valentine",
-      },
-      {
-        name: "🎃  halloween",
-        id: "halloween",
-      },
-      {
-        name: "🌷  garden",
-        id: "garden",
-      },
-      {
-        name: "🌲  forest",
-        id: "forest",
-      },
-      {
-        name: "🐟  aqua",
-        id: "aqua",
-      },
-      {
-        name: "👓  lofi",
-        id: "lofi",
-      },
-      {
-        name: "🖍  pastel",
-        id: "pastel",
-      },
-      {
-        name: "🧚‍♀️  fantasy",
-        id: "fantasy",
-      },
-      {
-        name: "📝  Wireframe",
-        id: "wireframe",
-      },
-      {
-        name: "🏴  black",
-        id: "black",
-      },
-      {
-        name: "💎  luxury",
-        id: "luxury",
-      },
-      {
-        name: "🧛‍♂️  dracula",
-        id: "dracula",
-      },
-      {
-        name: "🖨  CMYK",
-        id: "cmyk",
-      },
-      {
-        name: "🍁  Autumn",
-        id: "autumn",
-      },
-      {
-        name: "💼  Business",
-        id: "business",
-      },
-      {
-        name: "💊  Acid",
-        id: "acid",
-      },
-      {
-        name: "🍋  Lemonade",
-        id: "lemonade",
-      },
-      {
-        name: "🌙  Night",
-        id: "night",
-      },
-      {
-        name: "☕️  Coffee",
-        id: "coffee",
-      },
-      {
-        name: "❄️  Winter",
-        id: "winter",
-      },
-    ];
+  switchThemeHandler(): void {
+    this.themeSwitch = !this.themeSwitch;
+    localStorage.setItem("theme", this.themeSwitch ? "dark" : "light");
+    document.documentElement.setAttribute('data-theme', this.themeSwitch ? "dark" : "light");
   }
 }
